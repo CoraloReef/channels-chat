ba7d05fe467fdba647eb6a125bc4789c34ccc255
Viacheslav Gordeiko <slav78rus@gmail.com>
2019-09-02 23:50:36 +0300

    Fix test

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,18 +2,25 @@ import reducers from '../src/reducers';
 
 test('reducers', () => {
   const state = reducers({
+    messages: { byId: {}, allIds: [] },
     channels: {
       byId: {
         1: { id: 1, name: 'general', removable: false },
-        2: { id: 2, name: 'random', removable: false },
+        2: { id: 2, name: 'random', removable: false }
       },
       allIds: [2, 1],
     },
-    currentChannel: '',
+    currentChannel: 1,
+    form: {},
   },
-  { type: 'CURRENT_CHANNEL_SET', payload: { id: 1 } });
+  {
+    type: '@@redux-form/REGISTER_FIELD',
+    meta: { form: 'newMessage' },
+    payload: { name: 'message', type: 'Field' },
+  });
 
   expect(state).toEqual({
+    messages: { byId: {}, allIds: [] },
     channels: {
       byId: {
         1: { id: 1, name: 'general', removable: false },
@@ -22,5 +29,12 @@ test('reducers', () => {
       allIds: [2, 1],
     },
     currentChannel: 1,
+    form: {
+      newMessage: {
+        registeredFields: {
+          message: { name: 'message', type: 'Field', count: 1 },
+        },
+      },
+    },
   });
 });
