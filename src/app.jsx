517376ae8d46047cb533bcb431cf4ad6cb7fb771
Viacheslav Gordeiko <slav78rus@gmail.com>
2019-09-14 15:35:39 +0300

    Update Eslint config

--- src/app.jsx
+++ src/app.jsx
@@ -23,6 +23,7 @@ export default ({ channels, messages, currentChannelId }) => {
     reducers,
     compose(
       applyMiddleware(thunk),
+      // eslint-disable-next-line no-underscore-dangle
       window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
     ),
   );
